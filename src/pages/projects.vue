799ea8497d726e8063a2fd6be4822bbaa0ab8b3b
alinex <info@alinex.de>
2018-04-03 19:31:14 +0200

    Add website project

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -6,6 +6,58 @@
     </div>
 
     <div class="area row">
+
+      <div>
+        <q-card inline>
+          <q-card-media overlay-position="top" class="relative-position">
+            <q-carousel autoplay="5100" infinite arrows class="absolute" style="height: 100%; width: 100%">
+              <q-carousel-slide img-src="statics/projects/web-1.jpg" />
+              <q-carousel-slide img-src="statics/projects/web-2.jpg" />
+              <q-carousel-slide img-src="statics/projects/web-3.jpg" />
+              <q-carousel-slide img-src="statics/projects/web-4.jpg" />
+              <q-carousel-slide img-src="statics/projects/web-5.jpg" />
+              <q-carousel-slide img-src="statics/projects/web-6.jpg" />
+              <q-carousel-slide img-src="statics/projects/web-7.jpg" />
+              <q-carousel-slide img-src="statics/projects/web-8.jpg" />
+              <q-carousel-control slot="control-button" slot-scope="carousel" position="bottom-left" :offset="[5, 5]">
+                <q-btn round dense push color="primary"
+                  :icon="carousel.inFullscreen ? 'fullscreen_exit' : 'fullscreen'"
+                  @click="carousel.toggleFullscreen()" />
+              </q-carousel-control>
+            </q-carousel>
+            <img src="statics/projects/web-1.jpg">
+            <q-card-title slot="overlay">
+              Websites
+            </q-card-title>
+          </q-card-media>
+          <q-card-main class="relative-position down">
+            <q-btn fab color="primary" icon="mdi-arrow-down-thick" class="absolute"
+            @click="cardSwitch(10)" />
+          </q-card-main>
+          <q-card-main :class="card_10 ? 'q-mt-md q-pb-none' : 'hidden'">
+            <p>Websites for private, organisations and small companies. They are really different and individual.</p>
+            <p>I always use the technology of the time it is created, often with content management systems to change contents on their own.
+              Sometimes also together with logo design.<br/></p>
+            <p>HTML, JavaScript, Bootstrap, Joomla, Google Tools</p>
+          </q-card-main>
+          <q-card-separator />
+          <q-card-actions :class="card_10 ? 'float-right' : 'hidden'">
+            <q-btn no-caps flat icon="open in browser" color="primary" title="Art Organisation"
+              @click="openURL('http://kulturmuehle-rechberghausen.de')" />
+            <q-btn no-caps flat icon="open in browser" color="primary" title="VfB Fanclub"
+              @click="openURL('http://www.schwaben-piranhas.de')" />
+            <q-btn no-caps flat icon="open in browser" color="primary" title="Small Company"
+              @click="openURL('http://freytag-finanzberatung.de')" />
+            <q-btn no-caps flat icon="open in browser" color="primary" title="Small Company"
+              @click="openURL('http://steinmetz-sartorius.de')" />
+            <q-btn no-caps flat icon="open in browser" color="primary" title="Holiday Appartments"
+              @click="openURL('http://ferienwohnungspanja.de')" />
+            <q-btn no-caps flat icon="open in browser" color="primary" title="Music Organisation"
+              @click="openURL('http://www.lichtensteinerblasmusik.de')" />
+          </q-card-actions>
+        </q-card>
+      </div>
+
       <div>
         <q-card inline>
           <q-card-main overlay-position="top" class="relative-position">
@@ -31,21 +83,15 @@
             @click="cardSwitch(9)" />
           </q-card-main>
           <q-card-main :class="card_9 ? 'q-mt-md q-pb-none' : 'hidden'">
-            <p>universal device independent administration interface with frontend and rest server base.</p>
+            <p>Universal device independent administration interface with frontend and REST server base.</p>
             <p>
               (2017-2018)</p>
-            <p>JavaScript ES6, HTML, CSS; NodeJS, Express; MongoDB</p>
+            <p>JavaScript ES6, HTML, CSS, Vue, Electron; NodeJS, Express; MongoDB</p>
           </q-card-main>
-          <q-card-separator />
-          <q-card-actions :class="card_9 ? 'float-right' : 'hidden'">
-            <q-btn no-caps flat icon="open in browser" color="primary" label="Open Application"
-              @click="openURL('http://inmedea-simulator.net')" />
-          </q-card-actions>
         </q-card>
       </div>
 
 <!--
-8 Alinex Admin
 8 DBReport, Mailman, Scripter
 -->
       <div>
@@ -117,13 +163,6 @@
         </q-card>
       </div>
 
-<!--
-5 Code Manager
-4 Webhoch3 CMS
-3 Alinex PHP
-2 Brokat Intranet Systems
-1 Organisation Manager
--->
       <div>
         <q-card inline>
           <q-card-media overlay-position="top" class="relative-position">
@@ -143,9 +182,9 @@
           </q-card-media>
           <q-card-main class="relative-position down">
             <q-btn fab color="primary" icon="mdi-arrow-down-thick" class="absolute"
-            @click="cardSwitch(3)" />
+            @click="cardSwitch(5)" />
           </q-card-main>
-          <q-card-main :class="card_3 ? 'q-mt-md q-pb-none' : 'hidden'">
+          <q-card-main :class="card_5 ? 'q-mt-md q-pb-none' : 'hidden'">
             <p>Server rendered web application based on modular PHP system for easy development of flexible applications.</p>
             <p>I started this and made lots of modules all completely tested and completely API documented but I got lost in it making all modules as flexible as possible and didn't completely finish the whole suite.
               (20ß9-2013)</p>
@@ -153,6 +192,13 @@
           </q-card-main>
         </q-card>
       </div>
+
+<!--
+4 Code Manager
+3 Webhoch3 CMS
+2 Brokat Intranet Systems
+1 Organisation Manager
+-->
 
     </div>
 
@@ -187,7 +233,8 @@ export default {
     card_6: false,
     card_7: false,
     card_8: false,
-    card_9: false
+    card_9: false,
+    card_10: false
   }),
   methods: {
     openURL,
